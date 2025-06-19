84ee9b90fa3b4999f971ebdc04c6d853497e5e83
osbo <osbo@mit.edu>
2025-06-19 22:57:19 +0200

    Initial Commit

---
+++ avante/avanteApp.swift
@@ -0,0 +1,17 @@
+//
+//  avanteApp.swift
+//  avante
+//
+//  Created by Carl Osborne on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct avanteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ avanteTests/avanteTests.swift
@@ -0,0 +1,17 @@
+//
+//  avanteTests.swift
+//  avanteTests
+//
+//  Created by Carl Osborne on 6/19/25.
+//
+
+import Testing
+@testable import avante
+
+struct avanteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
